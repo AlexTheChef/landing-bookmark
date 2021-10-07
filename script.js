bed40c9f0640154cfaffb6a606131afa62438f2f
XxMaxFateXx <aleksa.kumric@gmail.com>
2021-10-07 16:08:10 +0200

    DOne desktop design with some javascript

---
+++ script.js
@@ -0,0 +1,40 @@
+this.addEventListener("DOMContentLoaded", () => {
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () => {
+
+        if (question.parentNode.classList.contains("active")) {
+            question.parentNode.classList.toggle("active")
+        }
+        else {
+            questions.forEach(question => question.parentNode.classList.remove("active"))
+            question.parentNode.classList.add("active")
+        }
+    }))
+})
+
+function validation(event) {
+    event.preventDefault()
+
+    let email = document.getElementById("email").value;
+    let errorEmail = document.getElementById("error-email");
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email == "") {
+        errorEmail.innerHTML = "Email cannot be empty"
+        document.querySelector(".error-container").style.display = " block";
+        let err = document.getElementById("email")
+        err.classList.add("error-input")
+    }
+    else {
+        if (email.match(pattern)) {
+            errorEmail.innerHTML = ""
+            document.querySelector(".error-container").style.display = "none"
+        }
+        else {
+            errorEmail.innerHTML = "Whoops, make sure it's an email"
+            document.querySelector(".error-container").style.display = "block"
+        }
+    }
+
+    return false
+}
